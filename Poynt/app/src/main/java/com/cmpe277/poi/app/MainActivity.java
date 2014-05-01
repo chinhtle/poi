d53ab61371509d3ee7345f7b1f12e19a4087a829
Chinh Le <chinh.t.le@gmail.com>
2014-04-30 18:59:06 -0700

    Refactored POI and Directions.  Moved POIs to MainActivity.

--- Poynt/app/src/main/java/com/cmpe277/poi/app/MainActivity.java
+++ Poynt/app/src/main/java/com/cmpe277/poi/app/MainActivity.java
@@ -5,6 +5,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.ActionBar;
 import android.app.Activity;
@@ -17,6 +20,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -25,6 +29,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.util.Log;
@@ -33,21 +38,49 @@ import android.widget.AutoCompleteTextView;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends Activity {
+    private final static String TAG = "MainActivity";
+
     PoyntLocationManager poyntLocation;
     private final static boolean LOCATION_UPDATE_ON_CREATE = true;
     private final static boolean LOCATION_SINGLE_UPDATE = true;
 
     private AutoCompleteTextView from;
     private AutoCompleteTextView to;
+    private ListView poiListView;
     private String directions = null;
+
+    private ArrayList<String> listShow = new ArrayList<String>();
+    private ArrayList<String> locationList = new ArrayList<String>();
+    private ArrayList<String> resultList = new ArrayList<String>();
+    private Map<String,String> finalMap = new HashMap<String, String>();
+    //private Set<String> finalSet = new HashSet<String>();
+
+    private final String API_KEY="AIzaSyCDqRAPjBga2wA0zwVzQpfElhn_ZqmLJ1Q";
+    private final String URL="https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+    private final String TYPES="store";
+    private final String RADIUS="500";
+    private final String SENSOR="false";
+
+    private enum PostDirExecType {
+        POST_EXEC_TYPE_UNKNOWN,
+        POST_EXEC_TYPE_POI,
+        POST_EXEC_TYPE_DIRECTIONS
+    };
+
+    private PostDirExecType execType = PostDirExecType.POST_EXEC_TYPE_UNKNOWN;
 
     // TODO
     // private DestListAdapter destListAdapter;
@@ -78,20 +111,10 @@ public class MainActivity extends Activity {
         from.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.list_items));
         to = (AutoCompleteTextView) findViewById(R.id.to);
         to.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.list_items));
+        poiListView = (ListView) findViewById(R.id.poiListView);
 
-        // Set action listeners for the to/from textfields for "search" or "done" from keyboard
-        // event.
-        from.setOnEditorActionListener(new TextView.OnEditorActionListener(){
-            @Override
-            public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
-                if((arg1 == EditorInfo.IME_ACTION_SEARCH) ||
-                   (arg1 == EditorInfo.IME_ACTION_DONE))
-                {
-                    // search pressed and perform your functionality.
-                    loadDirections(arg0);
-                }
-                return false;
-        } });
+        // Set action listeners for the to textfield for "search" or "done" from keyboard
+        // event.  Invokes retrieving POIs
         to.setOnEditorActionListener(new TextView.OnEditorActionListener(){
             @Override
             public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
@@ -99,13 +122,247 @@ public class MainActivity extends Activity {
                    (arg1 == EditorInfo.IME_ACTION_DONE))
                 {
                     // search pressed and perform your functionality.
-                    loadDirections(arg0);
+                    getPois();
                 }
                 return false;
         } });
     }
 
-    public void loadDirections(View view){
+    public boolean validateLocationFields()
+    {
+        boolean success = true;
+
+        if(!((from.getText().length() > 0) && (to.getText().length() > 0)))
+        {
+            // Display an error
+            Toast.makeText(this, "All fields must be filled out", Toast.LENGTH_LONG).show();
+            success = false;
+        }
+
+        Log.v(TAG, "Validation successful: " + success);
+        return success;
+    }
+
+    public void getDirections()
+    {
+        // First we need to validate the to and from fields!
+        if(validateLocationFields())
+        {
+            // Load the directions string into local buffer
+            loadDirections();
+        }
+    }
+
+    public void getPois()
+    {
+        Log.v(TAG, "Entry point of retrieving the POIs.");
+        execType = PostDirExecType.POST_EXEC_TYPE_POI;
+        getDirections();
+    }
+
+    public void executeGetPoisPostDirections()
+    {
+        JSONArray jArr;
+        ArrayList<String> urlList = new ArrayList<String>();
+        //   Map<KeyPOI, String> mapPOIs = new HashMap<KeyPOI, String>();
+
+        //gets https urls for each location list
+        if (locationList.isEmpty()) {
+            listShow.add("No POIs Available.");
+        } else {
+            jArr = getJsonLngLat(locationList);
+            for (int i = 0; i < jArr.length(); i++) {
+                Log.i("lSize", String.valueOf(jArr.length()));
+                try {
+                    urlList.add(getHttpsUrl(jArr.getJSONObject(i)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            new GetPOIs().execute(urlList);
+        }
+    }
+
+    private JSONArray getJsonLngLat(ArrayList<String> list){
+        JSONArray jArr = new JSONArray();
+
+        for(String s: list) {
+            try {
+                JSONObject jobj = new JSONObject(s);
+                jArr.put(jobj);
+                //
+                Log.i("slocationList",s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return jArr;
+    }
+
+    private String getHttpsUrl(JSONObject jObj){
+        StringBuilder sb = new StringBuilder();
+        sb.append(URL);
+        sb.append("location=");
+
+        String lng=null;
+        String lat=null;
+        try {
+            lng = String.valueOf(jObj.getDouble("lng"));
+            lat = String.valueOf(jObj.getDouble("lat"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        sb.append(lat+",");
+        sb.append(lng+"&radius=");
+
+        sb.append(RADIUS+"&types=");
+        sb.append(TYPES+"&sensor=");
+        sb.append(SENSOR+"&key=");
+        sb.append(API_KEY);
+
+        return sb.toString();
+    }
+
+    private void getValuesPOI(String result){
+
+        try {
+            JSONObject jObj = new JSONObject(result);
+
+            if(jObj.has("error_message")){
+                String msg1 = jObj.getString("error_message"+" Free tier allows 1000 calls in 24 hours");
+                String msg2 = jObj.getString("status");
+
+                Toast.makeText(getApplicationContext(),msg1+"\n"+msg2,Toast.LENGTH_LONG).show();
+                listShow.add(msg1);
+                listShow.add(msg2);
+
+            }
+
+            JSONArray jArr = jObj.getJSONArray("results");
+
+            for (int k=0; k<jArr.length();k++){
+                JSONObject jObj2 = jArr.getJSONObject(k);
+                String id = jObj2.getString("id");
+                if (!finalMap.containsKey(id)) {
+
+                    ///
+                    String open = jObj2.getJSONObject("opening_hours").getString("open_now");
+                    if (open.equals("true"))
+                        open = "Yes";
+                    else
+                        open = "No";
+
+
+                    StringBuilder poiSb = new StringBuilder();
+                    poiSb.append(jObj2.getString("name") + "\n");
+                    poiSb.append(jObj2.getString("vicinity") + "\n");
+                    poiSb.append("Rating: " + jObj2.getString("rating") + "\n");
+                    poiSb.append("Open Now: " + open);
+                    String poisNew = poiSb.toString();
+                    ///
+
+                    finalMap.put(id,poisNew);
+                }
+            }
+//            for(String s: finalSet){
+//                Log.i("fs",s);
+//            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private class GetPOIs extends AsyncTask<ArrayList<String>, Void, String> {
+
+
+        @Override
+        protected String doInBackground(ArrayList<String>... params) {
+
+            ArrayList<String> tempList = new ArrayList<String>();
+            tempList = params[0];
+            Log.i("number",String.valueOf(tempList.size()));
+            for (int i=0; i<tempList.size(); i++) {
+                try {
+                    StringBuilder jsonResults = new StringBuilder();
+                    URL url = new URL(tempList.get(i));
+                    Log.i("urlafter",tempList.get(i));
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                    int read;
+                    char[] buff = new char[1024];
+                    while ((read = in.read(buff)) != -1) {
+                        jsonResults.append(buff, 0, read);
+                    }
+                    getValuesPOI(jsonResults.toString());
+
+                    in.close();
+                    conn.disconnect();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            for(Map.Entry<String,String> entry: finalMap.entrySet()){
+                listShow.add(entry.getValue());
+            }
+
+            return null ;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            if(!listShow.isEmpty()) {
+                poiListView.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                       android.R.layout.simple_list_item_1, listShow));
+            }
+        }
+    }
+
+    public void parseDirectionsToLists()
+    {
+        try {
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(directions);
+            JSONArray routesJsonArray = jsonObj.getJSONArray("routes");
+
+            // Extract the instructions and end locations from the results
+            //Getting legs JSONArray
+            for(int i = 0; i < routesJsonArray.length(); i++) {
+                JSONObject routesJsonObject = routesJsonArray.getJSONObject(i);
+                JSONArray legsJsonArray = routesJsonObject.getJSONArray("legs");
+                //Getting each steps JSONArray
+                for(int j = 0; j < legsJsonArray.length(); j++){
+                    JSONObject legsJsonObject = legsJsonArray.getJSONObject(j);
+                    JSONArray stepsJsonArray = legsJsonObject.getJSONArray("steps");
+                    resultList = new ArrayList<String>(stepsJsonArray.length());
+                    locationList = new ArrayList<String>(stepsJsonArray.length());
+                    //Getting html_instructions and end_location from each steps array
+                    for (int k = 0; k < stepsJsonArray.length(); k++) {
+                        String tempHtmlInstructions = Html.fromHtml(stepsJsonArray.getJSONObject(k).getString("html_instructions")).toString();
+                        resultList.add(tempHtmlInstructions);
+                        locationList.add(stepsJsonArray.getJSONObject(k).getString("end_location"));
+                    }
+                }
+            }
+            ///
+            for (String s: locationList){
+                Log.i("locationList",s);
+            }
+        } catch (JSONException e) {
+            Log.v(TAG, "Cannot process JSON results", e);
+        }
+    }
+
+    public void loadDirections() {
+        Log.v(TAG, "Loading directions..");
         new requestDirections().execute();
     }
 
@@ -113,7 +370,8 @@ public class MainActivity extends Activity {
     {
         @Override
         protected String doInBackground(Void... arg0) {
-            String LOG_TAG = "MainActivity";
+            Log.v(TAG, "Requesting directions in background..");
+
             HttpURLConnection conn = null;
             StringBuilder jsonResults = new StringBuilder();
             try {
@@ -131,11 +389,11 @@ public class MainActivity extends Activity {
                 while ((read = in.read(buff)) != -1) {
                     jsonResults.append(buff, 0, read);
                 }
-                Log.v(LOG_TAG, "Directions results: " + jsonResults.toString());
+                Log.v(TAG, "Directions results: " + jsonResults.toString());
             } catch (MalformedURLException e) {
-                Log.v(LOG_TAG, "Error processing Directions API URL", e);
+                Log.v(TAG, "Error processing Directions API URL", e);
             } catch (IOException e) {
-                Log.v(LOG_TAG, "Error connecting to Directions API", e);
+                Log.v(TAG, "Error connecting to Directions API", e);
             } finally {
                 if (conn != null) {
                     conn.disconnect();
@@ -146,9 +404,28 @@ public class MainActivity extends Activity {
         }
         protected void onPostExecute(String result) {
             directions = result;
-            Intent intent = new Intent(MainActivity.this, DirectionsActivity.class);
-            intent.putExtra("resultList", directions);
-            startActivity(intent);
+
+            // Parse the obtained JSON results
+            parseDirectionsToLists();
+
+            switch(execType)
+            {
+                case POST_EXEC_TYPE_POI:
+                    executeGetPoisPostDirections();
+                    break;
+                case POST_EXEC_TYPE_DIRECTIONS:
+                {
+                    Intent intent = new Intent(MainActivity.this, DirectionsActivity.class);
+                    intent.putStringArrayListExtra("resultList", resultList);
+                    startActivity(intent);
+
+                    break;
+                }
+                default:
+                    // Unhandled error case, do nothing.
+                    Log.v(TAG, "Received unhandled exec type on post directions execution.");
+                    return;
+            }
         }
 
     }
@@ -170,7 +447,8 @@ public class MainActivity extends Activity {
 
     public void onClickDirections(View view)
     {
-        // TODO
+        execType = PostDirExecType.POST_EXEC_TYPE_DIRECTIONS;
+        getDirections();
     }
 
     public void onClickFavorites(View view)
